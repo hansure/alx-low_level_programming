737da52854d01ac6fc2cf77216552acbc5216de2
Surafel <surafelmelese123@gmail.com>
2022-06-30 11:49:56 -0700

    First commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@ char c;
 long int d;
 long long int e;
 
-printf("Size of integer: %lu byte(s)\n", (unsigned long)sizeof(a))
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(b))
-printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c))
-printf("Size of long: %lu byte(s)\n", (unsigned long)sizeof(d))
-printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(e))
-return(0)
+printf("Size of integer: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of long: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(e));
+return(0);
 }
