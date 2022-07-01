bfa2a71e8e090ef51e4fb4ee40f9ab0206dcc134
Surafel <surafelmelese123@gmail.com>
2022-07-01 02:10:55 -0700

    Variable if-else and loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+int main(void)
+{
+int n;
+
+for (n='a'; n <= 'z'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+int n;
+for(n='a'; n<='z'; n++)
+{
+putchar(n);
+}
+for(n='A'; n<='Z'; n++)
+{
+putchar(n);
+}
+
+putchar('\n');
+	return (0);
+}
