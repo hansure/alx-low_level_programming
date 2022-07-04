f8b89f5e4d7c11a08c5d6a9e70bfd19461c0c495
Surafel <surafelmelese123@gmail.com>
2022-07-04 11:45:51 -0700

    Basic c language

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 /*
- * This function to display alphabetical letters*/
+ * This function to display alphabetical letters
+ * */
 int main(void)
 {
 int n;
 for (n = 'a'; n <= 'z'; n++)
 {
-if(n == 'e' || n == 'q')
+if (n == 'e' || n == 'q')
 {
 continue;
 }
-else {
+else
+{
 putchar(n);
 }
 }
