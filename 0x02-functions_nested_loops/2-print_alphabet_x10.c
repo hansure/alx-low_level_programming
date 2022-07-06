df8370e777ab8c3b5bd96d9db4aa8c1f0d4ccd0b
Surafel <surafelmelese123@gmail.com>
2022-07-06 06:20:57 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * Documentation goes here
+
+*/
+
+int main(void)
+{
+int n;
+int i;
+for (i=0;i<10;i++)
+{
+for (n='a';n<='z'; n++)
+{
+_putchar(n+'0');
+}
+_putchar('\n');
+}
+return (0);
+}
