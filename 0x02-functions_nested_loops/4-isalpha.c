d6ee20b0ae6182b2513e066753f233ae1e0fa5da
Surafel <surafelmelese123@gmail.com>
2022-07-06 12:24:44 -0700

    0x02 C Function, nested loop mandatory questions  done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,9 +5,9 @@
  */
 
 
-int _isalpha(int ch)
+int _isalpha (int ch)
 {
-if(((ch >= 'a')&& (ch <='z')) || ((ch >= 'A') &&(ch <= 'Z')))
+if (((ch >= 'a') && (ch <='z')) || ((ch >= 'A') && (ch <= 'Z')))
 	return (1);
 else
 	return (0);
