3a3077409d5320d6ce54ad0e86e568336ba39f1b
Surafel <surafelmelese123@gmail.com>
2022-07-14 13:47:08 -0700

    More functions, More nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_line: Drawing a straight line
+ * n: The number of lines
+ *
+ * Return: empty
+ */
+void print_line(int n)
+{
+
+	int x;
+
+	if (n <= 0)
+	{
+	
+		_putchar('\n');
+	}
+
+	else
+	{
+	
+		for (x = 0; x < n; x++)
+		{
+		
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
