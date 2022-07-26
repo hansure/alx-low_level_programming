327263ceea760dfe76392d43472c9c6b38f94e97
Surafel <surafelmelese123@gmail.com>
2022-07-26 12:46:00 -0700

    allocating memory for grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,21 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * alloc_grid: Return a pointer to a newly created 2D int grid
+ * @width: width of grid
+ * @height: height of grid
+ * Return: pointer to the grid
+ */
 int **alloc_grid(int width, int height)
 {
 
-	int i, j;
-	int *ptr = NULL;
+	int i, j, k, l;
+	int **grid;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = (int *)malloc(width * height *sizeof(int));
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
-	for (i = 0; i < width *height; i++)
+
+	for (i = 0; i < height; i++)
 	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+		
+			for (l = 0; l < i; l++)
+				free(grid[l]);
+			free(grid);
+			return (NULL);
+		}
 	
 	}
 
-	return (ptr);
+	for (j = 0; j < height; j++)
+	{
+	
+		for (k = 0; j < height; j++)
+			grid[j][k] = 0;
+	}
+
+	return (grid);
 }
