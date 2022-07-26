81cb5ac62ae911c09a15c528904189d0698c3150
Surafel <surafelmelese123@gmail.com>
2022-07-26 12:16:17 -0700

    Memory allocation and pointer

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,12 +7,12 @@ char *str_concat(char *s1, char *s2)
 
 
 	int i, j, k;
-	char *newStr = NULL;
+	char *newPtr = NULL;
 
-	if (*s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
