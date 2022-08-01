9e51167aafc1cc4454f282e0cd9d5b97308c3283
Surafel <surafelmelese123@gmail.com>
2022-08-01 12:16:23 -0700

    Structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * * struct dog: Dog attributes
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+struct dog {
+
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
