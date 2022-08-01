6ff9ce2f5ea376907949378baf75cd86119bfba5
Surafel <surafelmelese123@gmail.com>
2022-08-01 11:57:53 -0700

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef _abs_h_
-#define _abs_h
-#define ABS(x) (((x) > (0)) ? ((x) * (-1)) : (x)) 
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h
 
-#endif /* _abs_h_ */
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x)) 
+
+#endif /* _function_like_macro_h_ */
