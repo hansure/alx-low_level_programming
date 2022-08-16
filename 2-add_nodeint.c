91dd9ce070629f291b5412d6e9fc1fa463b78d9f
Surafel <surafelmelese123@gmail.com>
2022-08-16 12:25:06 -0700

    Linked list commit

---
+++ 2-add_nodeint.c
@@ -0,0 +1,20 @@
+/**
+ * add_nodeint: Adds a new node at the beginning
+ * @head: A pointer to the address of the head
+ * @n: The integer for the new node to contain
+ *
+ * Return: If the function fails if NULL otherwise the address of the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
+	listint_t *new;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
